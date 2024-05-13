8d4c8d5b0f25abeed7106666a2880f3b6b8c527b
DanielB566 <Danib7382@gmail.com>
2024-05-13 10:19:12 -0400

    declaring variables

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
 // CODE EXPLAINED channel
+
+//Select the Elements
+const clear = document.querySelector(".clear");
+const dataElement = document.getElementById("date");
+const list = document.getElementById("list");
+const input = document.getElementById("input");
+
+// Class names
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle-thin";
+const LINE_THROUGH = "lineThrough";
